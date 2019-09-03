10966d6f8532ba0e9ab25480a44beb5a3012f2ec
Ed Pozharski <pozharski@gmail.com>
2019-09-03 19:05:21 -0400

    Further updates to KDCalc

--- scripts/kdcalc.py
+++ scripts/kdcalc.py
@@ -6,17 +6,45 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from matplotlib.figure import Figure
 from matplotlib.pyplot import grid
+from scipy.stats import rankdata
+from scipy import sqrt
+
 class KDWindow(Figure):
     def __init__(self, *args, **kwds):
         Figure.__init__(self, *args, **kwds)
+        self.pt={}
     def set_data(self, data):
-        self.Ish = data
+        self.Ish, self.d12, self.cc12, self.sp12 = data
+        self.target = 0
     def plot(self):
-        self.add_subplot(111)
+        self.add_subplot(221)
         self.axes[0].clear()
-        self.pt = self.axes[0].plot(self.Ish[19][0],self.Ish[19][1])
+        self.pt['cc12'] = self.axes[0].plot(1/self.d12**2,self.cc12,'bo',1/self.d12[self.target]**2,self.cc12[self.target]+0.1,'ro')
+        vals = 1/sqrt(self.axes[0].get_xticks())
+        self.axes[0].set_xticklabels(("%.2f "*len(vals) % tuple(vals)).split())
+        grid(True)
+        self.add_subplot(222)
+        self.axes[1].clear()
+        self.pt['sp12'] = self.axes[1].plot(1/self.d12**2,self.sp12,'go',1/self.d12[self.target]**2,self.sp12[self.target]+0.1,'ro')
+        vals = 1/sqrt(self.axes[1].get_xticks())
+        self.axes[1].set_xticklabels(("%.2f "*len(vals) % tuple(vals)).split())
+        grid(True)
+        self.add_subplot(223)
+        self.axes[2].clear()
+        self.pt['ish'] = self.axes[2].plot(self.Ish[self.target][0],self.Ish[self.target][1],'b.')
+        grid(True)
+        self.add_subplot(224)
+        self.axes[3].clear()
+        self.pt['ranked'] = self.axes[3].plot(rankdata(self.Ish[self.target][0]),rankdata(self.Ish[self.target][1]),'g.')
         grid(True)
         self.canvas.draw()
+    def onkeypress(self, event):
+        if event.key == 'right':
+            self.target = min(self.target+1, len(self.d12)-1)
+            self.plot()
+        if event.key == 'left':
+            self.target = max(self.target-1, 0)
+            self.plot()
 
 def main():
     from argparse import ArgumentParser, RawDescriptionHelpFormatter
@@ -58,7 +86,7 @@ def main():
     print("Will subdivide data into %d resolution shells" % Nshells)
     edges = dataset.GetResolutionShells(Nshells)
     shell_column = dataset.GetShellColumn(edges)
-    cc12, sp12, p12, Ish = [], [], [], []
+    d12, cc12, sp12, p12, Ish = [], [], [], [], []
     for i in range(Nshells):
         Ish1, Ish2 = [], []
         shind = (shell_column == i)
@@ -77,10 +105,12 @@ def main():
         sp12.append(sp12t)
         p12.append(p12t)
         Ish.append([Ish1,Ish2])
-        print("%4d %7.2f %7.2f %7.2f %8d %5.3f %5.3f %6.3g" % (i+1, edges[i], edges[i+1], dataset.GetResolutionColumn()[shind].mean(), sum(shind), cc12[-1], sp12t, p12t))
+        d12.append(dataset.GetResolutionColumn()[shind].mean())
+        print("%4d %7.2f %7.2f %7.2f %8d %5.3f %5.3f %6.3g" % (i+1, edges[i], edges[i+1], d12[-1] , sum(shind), cc12[-1], sp12t, p12t))
     Ish_fig = figure(FigureClass=KDWindow)
-    Ish_fig.set_data(Ish)
+    Ish_fig.set_data([Ish,array(d12),cc12,sp12])
     Ish_fig.plot()
+    Ish_fig.canvas.mpl_connect('key_press_event', Ish_fig.onkeypress)
     show()
 
 if __name__ == "__main__":
